55a9a9ff80bcc234563def9f471d7a76220d12b8
yalhasan <yalhasan13@gmail.com>
2023-10-07 13:57:40 +0300

    game working

--- src/components/Clicker.js
+++ src/components/Clicker.js
@@ -2,46 +2,45 @@ import React, { useState } from "react";
 
 let buildings = [];
 let numberOfBuildings = 0;
+let number = 0;
+let b = buildings.forEach((building) => {
+  building += building;
+});
 
 const Clicker = () => {
   const [counter, setCounter] = useState(0);
   const [gold, setGold] = useState(0);
   const [building, setBuilding] = useState(10);
-
   const [level, setLevel] = useState(0);
+  const [increment, setIncrement] = useState(0);
 
   const addPoints = () => {
-    if (numberOfBuildings == 0) {
+    if (level == 0) {
       setCounter(counter + 1);
       setGold(gold + 1);
-    } else {
-      setCounter(counter + buildings.length + 1);
-      setGold(gold + buildings.length + 1);
-    }
-
-    if (level == 1) {
-      setCounter(counter + 2);
-    } else if (level == 2) {
-      setCounter(counter + 3);
-    } else if (level == 4) {
-      setCounter(counter + 4);
+    } else if (level >= 1) {
+      setCounter(counter + increment);
+      setGold(gold + increment);
+      console.log({ increment, counter });
     }
   };
 
   const buyBuilding = (e) => {
-    setLevel(e.target.value);
-    console.log(e.target.value);
-    if (gold >= 10) {
+    setLevel(e);
+    console.log(e);
+    if (gold >= building) {
       setGold(gold - building);
-      setBuilding(building);
       numberOfBuildings++;
-      buildings.push(numberOfBuildings);
+      setIncrement(increment + e);
     }
   };
 
   return (
     <div className="background">
       <div className="goldposition">
+        <div className="upgrade">
+          <h2>the number of upgrades you have is: {numberOfBuildings}</h2>
+        </div>
         <div className="gold">
           <img
             className="imgdesign"
@@ -58,35 +57,66 @@ const Clicker = () => {
         <button className="clicker" onClick={addPoints} value={1}>
           <h3>clicker</h3>
         </button>
+
         <div className="buyposition">
           {gold >= 10 && (
-            <button onClick={(e) => buyBuilding(e)} value={1}>
-              {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="70"
-              height="70"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="feather feather-home"
-            >
-              <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-              <polyline points="9 22 9 12 15 12 15 22" />
-            </svg> */}
-              Upgrade +2
+            <button className="button" onClick={() => buyBuilding(2)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="50"
+                height="50"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-home"
+              >
+                <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                <polyline points="9 22 9 12 15 12 15 22" />
+              </svg>
+              <h3> Upgrade +2</h3>
             </button>
           )}
           {gold >= 20 && (
-            <button onClick={(e) => buyBuilding(e)} value={2}>
-              Upgrade +3
+            <button className="button" onClick={() => buyBuilding(3)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="50"
+                height="50"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-home"
+              >
+                <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                <polyline points="9 22 9 12 15 12 15 22" />
+              </svg>
+              <h3> Upgrade +3</h3>
             </button>
           )}
           {gold >= 30 && (
-            <button onClick={(e) => buyBuilding(e)} value={4}>
-              Upgrade +4
+            <button className="button" onClick={() => buyBuilding(4)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="50"
+                height="50"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-home"
+              >
+                <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                <polyline points="9 22 9 12 15 12 15 22" />
+              </svg>
+              <h3> Upgrade +4</h3>
             </button>
           )}
         </div>
